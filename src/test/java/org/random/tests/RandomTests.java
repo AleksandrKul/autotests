a4f3faa8d0cac44307c4a46b299a6a0986008386
aleksandr <aleksandr.ku@miro.com>
2020-02-26 00:46:03 +0500

    Changes

--- src/test/java/org/random/tests/RandomTests.java
+++ src/test/java/org/random/tests/RandomTests.java
@@ -1,13 +1,19 @@
 package org.random.tests;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomTests {
+
+    static WebDriver driver;
 
     @Test
     void randomNumbersAreNotRepeatedTest() {
@@ -15,8 +21,12 @@ public class RandomTests {
         System.setProperty("webdriver.chrome.driver","libs/chromedriver");
 
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,10);
 
         driver.get("http://random.org");
+
+        wait.until(
+        ExpectedConditions.frameToBeAvailableAndSwitchToIt("#homepage-generator iframe"));
 
         driver.findElement(By.id("true-random-integer-generator-button")).click();
 
@@ -26,4 +36,8 @@ public class RandomTests {
 
     }
 
+    @AfterAll
+    static void afterAll() {
+        driver.quit();
+    }
 }
